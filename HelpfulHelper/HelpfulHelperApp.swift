46ff177aed07dce000586266c34ccf3fc50088e7
Sam Pullara <spullara@gmail.com>
2024-10-24 15:05:30 -0700

    Initial Commit

---
+++ HelpfulHelper/HelpfulHelperApp.swift
@@ -0,0 +1,32 @@
+//
+//  HelpfulHelperApp.swift
+//  HelpfulHelper
+//
+//  Created by Sam Pullara on 10/24/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HelpfulHelperApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ HelpfulHelperTests/HelpfulHelperTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelpfulHelperTests.swift
+//  HelpfulHelperTests
+//
+//  Created by Sam Pullara on 10/24/24.
+//
+
+import Testing
+@testable import HelpfulHelper
+
+struct HelpfulHelperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
